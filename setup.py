d05b5128579b04e57d33075fd0c0100c69fe9b72
Jean Ibarz <ibarz.jean@gmail.com>
2019-05-24 21:21:36 +0200

    Updated setup.py: updated author and install_requires (scipy,numpy).

--- setup.py
+++ setup.py
@@ -4,16 +4,16 @@ from setuptools import setup, find_packages
 
 setup(
     name="pySOFA",
-    version="0.0.3",
-    author="Joerg Encke",
+    version="0.0.1",
+    authors="Jean Ibarz",
     author_email="joerg.encke@tum.de",
 
     description="SOFA API",
     license="GPLv3+",
-    url="https://github.com/mrkrd/thorns",
+    url="https://github.com/mrkrd/thorns, https://github.com/jeanibarz/pySOFA",
     download_url="https://github.com/mrkrd/thorns/tarball/master",
     packages=find_packages(),
 
     platforms=["Linux", "Windows"],
-    install_requires=["tables"],
+    install_requires=["tables", "scipy", "numpy"],
 )
